cc75459c42f040480387a19be2ab47744f351a49
joemand985 <djordjemandic85@gmail.com>
2017-12-01 02:17:04 +0100

    add timer functions

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,7 +1,11 @@
 var numM = 0;
 var numS = 0;
 var num = 0;
+var pomodoros = 0;
+var set = 0;
+var rest = false;
 function timer(){
+    isBreak();
     var div = document.getElementById("timer");
     if (num > 10) {
         num = 0;
@@ -11,23 +15,44 @@ function timer(){
         numS = 0;
         numM++;
     }
-    div.innerHTML = numM + " " + numS + " "+ num;
-    num++
-    //if (numM > 25) {
-       // break();
-   //}
-}
-
-setInterval(timer, 100);
-
-/*function break() {
-    console.log("Kratka pauza");
-    reset();
+    if (rest){  // rest
+        rest_tone.play();
+        if(numM > 5){
+            rest = false;
+            reset();
+            work_tone.play();
+        }
+    }
+    if (numM > 25){ //end of work
+        reset();
+        pomodoros++
+        rest = true;
+        //work_tone.play();
+        //var rest = //documet.getElementById("work");
+       // rest.play();
     
+    }
+    div.innerHTML = numM + " : " + numS + " : "+ num;
+    num++
 }
 
-/*function reset() {
-    var numM = 0;
-    var numS = 0;
-    var num = 0;
+
+setInterval(timer, 30);
+
+  
+function reset(){
+    num, numS, numM = 0;
+}
+
+function isBreak() {
+    var isBreak ="Pomodoro";
+    if (rest){
+        isBreak = "Break";
+        document.getElementById("tomato").src='img/green.png';
+         document.getElementById("timer").color='red';
+    } else {
+        document.getElementById("tomato").src='img/red.png';
+        document.getElementById("timer").color='green';
+    }
+    document.getElementById("break").innerHTML = isBreak;
 }
