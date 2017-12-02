0cf1f13f6ac93043b867ba870388163f1c76b907
joemand985 <djordjemandic85@gmail.com>
2017-12-02 16:39:48 +0100

    finish timer

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,47 +1,68 @@
-var numM = 0;
+var numM = 0; 
 var numS = 0;
 var num = 0;
 var pomodoros = 0;
 var set = 0;
 var rest = false;
+var count = "";
+
+var breakLength = 0;
+var pomLength = 0;
+//var onOff = true;
 function timer(){
+   
     isBreak();
     var div = document.getElementById("timer");
-    if (num > 10) {
-        num = 0;
-        numS++;
+    if (num < 1) {
+        num = 100;
+        numS--;
     }
-    if (numS > 10){
-        numS = 0;
-        numM++;
+    if (numS < 1){
+        numS = 60;
+        numM--;
     }
-    if (rest){  // rest
-        rest_tone.play();
-        if(numM > 5){
+    if (rest){ 
+      
+        if(numM < 1){
             rest = false;
             reset();
-            work_tone.play();
+          
         }
     }
-    if (numM > 25){ //end of work
-        reset();
+    if (numM < 1){ //end of work
+        resetBreak();
         pomodoros++
-        rest = true;
-        //work_tone.play();
-        //var rest = //documet.getElementById("work");
-       // rest.play();
-    
+        rest = true;   
     }
-    div.innerHTML = numM + " : " + numS + " : "+ num;
-    num++
+    div.innerHTML = numM + " " + numS + " "+ num;
+    num--
+}
+
+function stop(){
+    clearInterval(count);
+}
+function start(){
+        breakLength = document.getElementById("break_length").value;
+        pomLength = document.getElementById("pomodoro_length").value;
+        count = setInterval(timer, 1);
+        numM = pomLength;
+     if (numM == 0){
+        alert("You didn't set session time!!!")
+    }
+
 }
 
 
-setInterval(timer, 30);
 
-  
+function resetBreak(){
+    numM = breakLength;
+    num, numS = 0;
+    rest_tone.play();
+} 
 function reset(){
-    num, numS, numM = 0;
+    numM = pomLength;    
+    num, numS = 0;
+    work_tone.play();
 }
 
 function isBreak() {
@@ -50,9 +71,38 @@ function isBreak() {
         isBreak = "Break";
         document.getElementById("tomato").src='img/green.png';
          document.getElementById("timer").color='red';
+        //rest_tone.play();
     } else {
         document.getElementById("tomato").src='img/red.png';
         document.getElementById("timer").color='green';
+        //work_tone.play();
     }
     document.getElementById("break").innerHTML = isBreak;
 }
+
+// --- To do list
+done = false;
+var items = [];
+var output = [];
+function add(){
+    var newItem = document.getElementById('input').value;
+    
+    items.push(newItem);
+    toDo();
+     console.log(items);
+    
+}
+
+function toDo(){
+    for(var i=0; i<items.length; i++){
+        output.push("<br>" +items + done)
+        items = [];
+    }
+    var list = document.getElementById('list');
+    list.innerHTML = output;
+}
+
+  
+   
+
+ 
